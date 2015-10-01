6f9ea20c03bb8795e415797f0fa537283837d022
Meng-Gen <plover@gmail.com>
2015-10-02 00:41:53 +0800

    Refactor all

--- report/chart.js
+++ report/chart.js
@@ -377,6 +377,101 @@ function makeCccChart(stockSymbol, period) {
     AmCharts.makeChart(div, config);
 }
 
+function makeDividendPolicy(stockSymbol) {
+    var div = makeDiv('dividend_policy');
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url": makeDataPath('dividend_policy', stockSymbol),
+            "format": "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size": 15,
+                "text": "股利政策",
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 2, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "axisAlpha" : 1,
+                "position" : "left",
+            }, 
+            {
+                "id" : "right_axis",
+                "axisThickness" : 2,
+                "gridAlpha" : 0,
+                "axisAlpha" : 1,
+                "position" : "right",
+                "title": "%",
+            },
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "left_axis",
+                "type" : "line", 
+                "title" : "現金股利",
+                "valueField" : "cash_dividends",
+                "bullet" : "round",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "股票股利",
+                "valueField" : "stock_dividends",
+                "bullet" : "square",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "盈餘配股",
+                "valueField" : "stock_dividends_from_retained_earnings",
+                "bullet" : "triangleUp",
+                "hidden" : true,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "公積配股",
+                "valueField" : "stock_dividends_from_capital_reserve",
+                "bullet" : "diamond",
+                "hidden" : true,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "right_axis",
+                "type" : "line",
+                "title" : "員工配股率 (%)",
+                "valueField" : "employee_stock_bonus_ratio",
+                "type" : "column",
+                "fillAlphas" : 0.1,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },        
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false, 
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
 function makeDupontChart(stockSymbol, period) {
     var div = makeDivByPeriod('dupont', period);
     var config = {
@@ -421,7 +516,7 @@ function makeDupontChart(stockSymbol, period) {
         ],
         "graphs" : [
             {
-                "valueAxis": "left_axis",
+                "valueAxis" : "left_axis",
                 "type" : "line", 
                 "title" : "ROE (%)",
                 "valueField" : "roe",
@@ -429,7 +524,7 @@ function makeDupontChart(stockSymbol, period) {
                 "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
             },
             {
-                "valueAxis": "left_axis",
+                "valueAxis" : "left_axis",
                 "type" : "line",
                 "title" : "ROA (%)",
                 "valueField" : "roa",
@@ -437,7 +532,7 @@ function makeDupontChart(stockSymbol, period) {
                 "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
             },
             {
-                "valueAxis": "left_axis",
+                "valueAxis" : "left_axis",
                 "type" : "line",
                 "title" : "純益率 (%)",
                 "valueField" : "ros",
@@ -445,7 +540,7 @@ function makeDupontChart(stockSymbol, period) {
                 "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
             },
             {
-                "valueAxis": "minor_left_axis",
+                "valueAxis" : "minor_left_axis",
                 "type" : "line",
                 "title" : "總資產週轉率 (%)",
                 "valueField" : "ato",
@@ -467,6 +562,359 @@ function makeDupontChart(stockSymbol, period) {
         },
         "chartCursor" : {
             "zoomable" : false, 
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
+function makeLiquidityChart(stockSymbol, period) {
+    var div = makeDivByPeriod('liquidity', period);
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url": makeDataPathByPeriod('liquidity', stockSymbol, period),
+            "format": "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size": 15,
+                "text": "償債能力分析" + makeTitleCommentText(period),
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 2, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "axisAlpha" : 1,
+                "position" : "left",
+                "title" : "%",
+            }, 
+            {
+                "id" : "right_axis",
+                "axisThickness" : 2,
+                "gridAlpha" : 0,
+                "axisAlpha" : 1,
+                "position" : "right",
+            },
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "left_axis",
+                "type" : "line", 
+                "title" : "流動比率 (%)",
+                "valueField" : "current_ratio",
+                "bullet" : "round",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "速動比率 (%)",
+                "valueField" : "quick_ratio",
+                "bullet" : "square",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "right_axis",
+                "type" : "line",
+                "title" : "利息保障倍數",
+                "valueField" : "interest_protection_multiples",
+                "bullet" : "diamond",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false, 
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
+function makeLongTermInvestmentsToAssetsRatioChart(stockSymbol, period) {
+    var div = makeDivByPeriod('long_term_investments_to_assets_ratio', period);
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url": makeDataPathByPeriod('cash_flow', stockSymbol, period),
+            "format": "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size": 15,
+                "text": "長期投資占總資產比率" + makeTitleCommentText(period),
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 0, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "axisAlpha" : 1,
+                "position" : "left",
+                "title" : "%",
+            }, 
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "left_axis",
+                "type" : "line", 
+                "title" : "長期投資占總資產比率 (%)",
+                "valueField" : "long_term_investments_to_assets_ratio",
+                "bullet" : "round",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false, 
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
+function makeNetIncomeToOperatingActivitiesChart(stockSymbol, period) {
+    var div = makeDivByPeriod('net_income_to_operating_activities', period);
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url": makeDataPathByPeriod('cash_flow', stockSymbol, period),
+            "format": "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size": 15,
+                "text": "會計盈餘與營運活動之現金流量之差異分析" + makeTitleCommentText(period),
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 0, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "axisAlpha" : 1,
+                "position" : "left",
+            }, 
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "left_axis",
+                "type" : "column", 
+                "title" : "營運活動之現金流量",
+                "valueField" : "cash_flow_from_operating_activities",
+                "fillAlphas" : 0.6,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "column",
+                "title" : "會計盈餘",
+                "valueField" : "net_profit",
+                "fillAlphas" : 0.6,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false, 
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
+function makeOperatingRevenueChart(stockSymbol) {
+    var div = makeDiv('operating_revenue');
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url" : makeDataPath('operating_revenue', stockSymbol),
+            "format" : "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size" : 15,
+                "text" : "每月營業收入分析",
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 0, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "maximum" : 20,
+                "axisAlpha" : 1,
+                "position" : "left",
+                "title" : "%",
+            }, 
+            {
+                "id" : "right_axis",
+                "axisThickness" : 2,
+                "gridAlpha" : 0,
+                "axisAlpha" : 1,
+                "position" : "right",
+            },
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "right_axis",
+                "type" : "column", 
+                "title" : "每月營收",
+                "valueField" : "operating_revenue",
+                "fillAlphas" : 0.1,
+                "hidden" : true,
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "累積營收年增率 (%)",
+                "valueField" : "accumulated_operating_revenue_yoy",
+                "fillAlphas" : 0.5,
+                "bullet" : "triangleUp",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "right_axis",
+                "type" : "line",
+                "title" : "長期平均線 (12ma)",
+                "valueField" : "long_term_average",
+                "bullet" : "round",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+            {
+                "valueAxis": "right_axis",
+                "type" : "line",
+                "title" : "短期平均線 (3ma)",
+                "valueField" : "short_term_average",
+                "bullet" : "square",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
+            },
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false,
+            "cursorPosition": "mouse",
+        },
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+    };
+    AmCharts.makeChart(div, config);
+}
+
+function makeProfitabilityChart(stockSymbol, period) {
+    var div = makeDivByPeriod('profitability', period);
+    var config = {
+        "type" : "serial",
+        "dataLoader": {
+            "url": makeDataPathByPeriod('profitability', stockSymbol, period),
+            "format": "json"
+        },
+        "categoryField" : "date",
+        "titles" : [
+            {
+                "size": 15,
+                "text": "獲利能力" + makeTitleCommentText(period),
+            },
+        ],
+        "numberFormatter" : {
+            "precision" : 2, 
+        },
+        "valueAxes" : [
+            {
+                "id" : "left_axis",
+                "axisThickness" : 2,
+                "axisAlpha" : 1,
+                "position" : "left",
+                "title" : "%",
+            }, 
+        ],
+        "graphs" : [
+            {
+                "valueAxis": "left_axis",
+                "type" : "line", 
+                "title" : "毛利率 (%)",
+                "valueField" : "gross_profit_margin",
+                "bullet" : "round",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "營業利益率 (%)",
+                "valueField" : "operating_profit_margin",
+                "bullet" : "square",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "稅前純益率 (%)",
+                "valueField" : "net_profit_before_tax_margin",
+                "bullet" : "triangleUp",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+            {
+                "valueAxis": "left_axis",
+                "type" : "line",
+                "title" : "稅後純益率 (%)",
+                "valueField" : "net_profit_margin",
+                "bullet" : "diamond",
+                "balloonText" : "<span style='font-size:14px; color:#000000;'><b>[[value]]%</b></span>",
+            },
+        ],
+        "legend" : {
+            "align" : "center",
+            "valueAlign" : "left",
+        },
+        "chartCursor" : {
+            "zoomable" : false,
             "cursorPosition": "mouse",
         },
         "balloon": {
@@ -569,7 +1017,17 @@ makeCashFlowChart(stockSymbol, 'yearly');
 makeCashFlowChart(stockSymbol, 'quarterly');
 makeCccChart(stockSymbol, 'yearly');
 makeCccChart(stockSymbol, 'quarterly');
+makeDividendPolicy(stockSymbol);
 makeDupontChart(stockSymbol, 'yearly');
 makeDupontChart(stockSymbol, 'quarterly');
+makeLiquidityChart(stockSymbol, 'yearly');
+makeLiquidityChart(stockSymbol, 'quarterly');
+makeLongTermInvestmentsToAssetsRatioChart(stockSymbol, 'yearly');
+makeLongTermInvestmentsToAssetsRatioChart(stockSymbol, 'quarterly');
+makeNetIncomeToOperatingActivitiesChart(stockSymbol, 'yearly');
+makeNetIncomeToOperatingActivitiesChart(stockSymbol, 'quarterly');
+makeOperatingRevenueChart(stockSymbol);
+makeProfitabilityChart(stockSymbol, 'yearly');
+makeProfitabilityChart(stockSymbol, 'quarterly');
 makeRevenueIndex(stockSymbol, 'yearly');
 makeRevenueIndex(stockSymbol, 'quarterly');
